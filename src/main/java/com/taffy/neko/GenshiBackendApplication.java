49ee561c3ed9a5fd0fca145bd89479b1f1c31ae6
Neon <840654613@qq.com>
2024-03-18 16:08:35 +0800

    配置跨域

--- src/main/java/com/taffy/neko/GenshiBackendApplication.java
+++ src/main/java/com/taffy/neko/GenshiBackendApplication.java
@@ -7,17 +7,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
+
 @SpringBootApplication
 @Slf4j
 @MapperScan("com.taffy.neko.mapper")
 public class GenshiBackendApplication {
-
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(GenshiBackendApplication.class, args);
         ConfigurableEnvironment environment = context.getEnvironment();
         String port = environment.getProperty("server.port");
         String serviceName = environment.getProperty("spring.application.name");
-        log.info("本项目已经在 " + port + " 端口启动(>^ω^<) 项目名称为: " + serviceName);
+        log.info("\n---------------------------------------------\n\t" +
+                "本项目已经在 " + port + " 端口启动(>^ω^<) \n\t" +
+                "项目名称为: " + serviceName + "\n\t"+
+                "接口文档地址: localhost:"+port+"/api/doc.html \n\t");
     }
 
 }
